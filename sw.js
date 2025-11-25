e2ccaf61cde4439cc70d205293b038dc0b97c80b
maulana isbad rofiqi <147914104+maulana-isbad-rofiqi@users.noreply.github.com>
2025-11-26 04:32:35 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'kebis-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Strategy (Network First, biar chat selalu update)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
